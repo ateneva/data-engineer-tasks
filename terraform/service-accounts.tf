1eedb0f126249e49858c5338c2b5eed96dc25ed9
ateneva <tenevaa21@gmail.com>
2024-09-23 16:56:37 +0200

    CU-86c0d90w8-ensure-service-account-can-write-to-both-storage- and-bigquery

--- terraform/service-accounts.tf
+++ terraform/service-accounts.tf
@@ -1,12 +1,12 @@
-
 module "service_accounts" {
   source        = "terraform-google-modules/service-accounts/google"
   version       = "~> 4.0"
   project_id    = "data-geeking-gcp"
-  prefix        = "storage"
+  prefix        = "storage-bq"
   names         = ["upload-data"]
   project_roles = [
-    "data-geeking-gcp=>roles/storage.admin"
+    "data-geeking-gcp=>roles/storage.admin",
+    "data-geeking-gcp=>roles/bigquery.admin"
   ]
   generate_keys = true
 }
