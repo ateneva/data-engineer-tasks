2609380538738ad0b72692e6c0d2c51667299e38
ateneva <tenevaa21@gmail.com>
2024-10-20 13:27:48 +0200

    CU-86c0qkrrd-optimize-executiion-times-of-zipped-to-jsonnl-method

--- plugins/helpers/csv_handling.py
+++ plugins/helpers/csv_handling.py
@@ -5,6 +5,7 @@ import csv
 import json
 import tempfile
 import logging
+import time
 from zipfile import ZipFile
 from dataclasses import dataclass
 
@@ -70,39 +71,48 @@ class CSVHandler:
             for file in myzip.namelist():
                 if file.endswith('.csv'):
                     with myzip.open(file) as f:
+                        data = f.read().decode(self.csv_encoding, 'ignore').splitlines()
                         with tempfile.NamedTemporaryFile('w', encoding=self.csv_encoding, delete=True):
-                            data = f.read().decode(self.csv_encoding, 'ignore').splitlines()
                             csv_reader = csv.DictReader(data, delimiter=self.csv_delimiter)
-                            logging.info('Constructing a Dictionary started')
+                            print('Constructing a Dictionary started')
                             json_data = [row for row in csv_reader]
+
+                        print('Conversion to new line delimited json started')
+                        with open(json_local_path, 'w', encoding='utf-8') as json_file:
+                            for record in json_data:
+                                if len(record) > 0:
+                                    json_file.write(json.dumps(record) + '\n')
+                        print(f'Converted file is {json_local_path}')
                 else:
                     raise OSError
-
-        logging.info('Conversion to new line delimited json started')
-        with open(json_local_path, 'w', encoding='utf-8') as json_file:
-            for record in json_data:
-                if len(record) > 0:
-                    json_file.write(json.dumps(record) + '\n')
-        logging.info(f'Converted file is {json_local_path}')
-
 
 # Example usage:
 
 data_engineer_jobs = CSVHandler('/Users/angelina.teneva/Documents/DataEngineer.csv', 'UTF-8', ',')
 data_engineer_jobs.change_csv_delimiters(
-    '/Users/angelina.teneva/Documents/data_engineer_jobs_tabbed.csv',
-    '|'
-)
+     '/Users/angelina.teneva/Documents/data_engineer_jobs_tabbed.csv',
+     '|'
+ )
 
 data_engineer_jobs.csv_to_pretty_json(
-    '/Users/angelina.teneva/Documents/data_engineer_jobs.json'
-)
+     '/Users/angelina.teneva/Documents/data_engineer_jobs.json'
+ )
 
 data_engineer_jobs.csv_to_new_line_delimited_json(
-    '/Users/angelina.teneva/Documents/data_engineer_jobs_new_line_delimited.json'
-)
+     '/Users/angelina.teneva/Documents/data_engineer_jobs_new_line_delimited.json'
+ )
 
 outbrain = CSVHandler('/Users/angelina.teneva/Downloads/gcp_datasets_Outbrain Kaggle Competition_page_views_sample.csv.zip', 'UTF-8', ',')
+
+start_time = time.time()
 outbrain.zipped_csv_to_new_line_delimited_json(
     '/Users/angelina.teneva/Documents/outbrain_comprehension_kaggle.json'
+)
+end_time = time.time()
+
+print(f'execution time: {end_time-start_time} seconds')
+
+netflix = CSVHandler('/Users/angelina.teneva/Downloads/gcp_datasets_zipped_1000-netflix-shows.zip', 'UTF-8', ',')
+netflix.zipped_csv_to_new_line_delimited_json(
+     '/Users/angelina.teneva/Documents/netflix_shows.json'
 )
